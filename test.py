6270ec1c73c12683e370d600aa6214de0f6c091d
Esa Varemo <esa@kuivanto.fi>
2019-08-11 19:21:05 +0300

    Add missing A-records

--- test.py
+++ test.py
@@ -1,8 +1,6 @@
 import ipahttp
-import json
 import pynetbox
-import random
-import string
+import re
 import subprocess
 
 ipa = ipahttp.ipa('ipa.tre.esav.fi', sslverify=True)
@@ -34,12 +32,92 @@ def get_addresses(nb):
         }
 
 
-# print((get_zones(ipa)))
-# print(json.dumps(list(get_addresses(nb))))
+# borrowed from https://stackoverflow.com/a/2532344/966508
+def is_valid_fqdn(hostname):
+    if len(hostname) > 255:
+        return False
+    if hostname[-1] == ".":
+        hostname = hostname[:-1]  # strip exactly one dot from the right, if present
+    allowed = re.compile("(?!-)[A-Z\d-]{1,63}(?<!-)$", re.IGNORECASE)
+    return all(allowed.match(x) for x in hostname.split("."))
 
-rand = ''.join(random.choice(string.ascii_lowercase) for i in range(5))
 
-r = ipa.dnsrecord_add('my.zone', f"automation-test-{rand}", {"arecord": ["2.3.4.5"]})
-print(json.dumps(r))
+def clean_dns_name(name):
+    name = name.strip()
+    if not name.endswith('.'):
+        name += '.'
+    return name
 
-print(json.dumps(ipa.dnsrecord_find('my.zone')))
+
+def find_zone(zones, name):
+    zone_match = ""
+    for zone in zones:
+        if name.endswith(zone):
+            if len(zone) > len(zone_match):
+                zone_match = zone
+
+    if len(zone_match) and len(zone_match) != len(name):
+        return zone_match
+
+    return None
+
+
+zones = get_zones(ipa)
+
+a_records = []
+ptr_records = []
+
+for address in get_addresses(nb):
+    if address['status'] != "Active":
+        continue
+
+    ip, prefix = address['address'].split('/')
+    if len(address['dns']):
+
+        name = clean_dns_name(address['dns'])
+
+        if not is_valid_fqdn(name):
+            continue
+
+        zone = find_zone(zones, name)
+        if not zone:
+            continue
+
+        a_records.append((zone, name, ip,))
+
+    if len(address['description']):
+        name = clean_dns_name(address['description'])
+
+        if not is_valid_fqdn(name):
+            continue
+
+        zone = find_zone(zones, name)
+        if not zone:
+            continue
+
+        ptr_records.append((zone, name, ip,))
+
+# print(a_records)
+# print(ptr_records)
+
+
+def get_host(fqdn, zone):
+    without_zone = fqdn[:fqdn.index(zone)]
+    assert without_zone[-1] == '.'
+    return without_zone[:-1]
+
+
+for zone in zones:
+    records = ipa.dnsrecord_find(zone)['result']['result']
+
+    for rec_zone, rec_name, rec_ip in a_records:
+        if rec_zone != zone:
+            continue
+        host = get_host(rec_name, rec_zone)
+
+        for record in records:
+            if record['idnsname'][0] == host:
+                break
+        else:
+            print(f"{host} in {zone} is missing a record. Creating now")
+            ipa.dnsrecord_add(zone, host, {"arecord": [rec_ip]})
